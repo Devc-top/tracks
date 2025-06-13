4df680ffd792c2f3bf88d2385366c4ce63737362
Devc-top <top.devc@gmail.com>
2025-06-13 09:35:48 -0700

    Update send-location.js

--- api/send-location.js
+++ api/send-location.js
@@ -1,16 +1,21 @@
 import nodemailer from 'nodemailer';
 
 export default async function handler(req, res) {
-  if (req.method !== 'POST') return res.status(405).send('Method Not Allowed');
+  if (req.method !== 'POST') {
+    return res.status(405).send('Method Not Allowed');
+  }
 
   const { latitude, longitude } = req.body;
 
-  if (!latitude || !longitude) return res.status(400).send('Latitude and longitude required');
+  if (!latitude || !longitude) {
+    return res.status(400).send('Latitude and longitude required');
+  }
 
-  if (!process.env.EMAIL_USER || !process.env.EMAIL_PASS)
+  if (!process.env.EMAIL_USER || !process.env.EMAIL_PASS) {
     return res.status(500).send('Email credentials not set');
+  }
 
-  let transporter = nodemailer.createTransport({
+  const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
       user: process.env.EMAIL_USER,
